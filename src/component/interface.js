44b72eaf31908b2ec1fe359fef39b5b622dfaeb1
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-05-02 11:24:18 +0530

    added box and input box

---
+++ src/component/interface.js
@@ -0,0 +1,15 @@
+import '../style/interface.css'
+const Interface = () => {
+    return ( 
+        <div className="interface">
+            <div className="box">
+               <div className="title">Dictionary</div>
+               <div className="input-box">
+                <input type="text" />
+               </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Interface;
