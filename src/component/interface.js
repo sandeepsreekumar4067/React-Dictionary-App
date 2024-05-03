98ca7eaf97427d7abd77fb9a79e7146b5a420ec3
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-05-03 14:23:44 +0530

    added animation to the result

--- src/component/interface.js
+++ src/component/interface.js
@@ -7,30 +7,37 @@ const Interface = () => {
     const [data,setdata] = useState(null)
     const [path,setpath] = useState(null)
     const [word,setword] = useState('')
+    const [handling,sethandling] = useState(false)
     const API = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
     const FetchData = () =>{
-        fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+word)
-        .then((response)=> {
-            if(response.ok){
-                return response.json()
-            }else{
-                throw new Error("Stop Typing Nonsense")
-            }
-        })
-        .then((data)=>{
-            if(data.length>0){
-                setpath(data[0].meanings[0].definitions[0].definition)
-            }else{
-                throw new Error("No Definition Found")
-            }
-        })
-        .catch((e)=>{
-            setpath(e.toString())
-        })
-        setword('')
+        if(word.length>0){
+            fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+word)
+            .then((response)=> {
+                if(response.ok){
+                    sethandling(true)
+                    return response.json()
+                }else{
+                    throw new Error("Stop Typing Nonsense")
+                }
+            })
+            .then((data)=>{
+                if(data.length>0){
+                    setpath(data[0].meanings[0].definitions[0].definition)
+                    
+                }else{
+                    throw new Error("No Definition Found")
+                }
+            })
+            .catch((e)=>{
+                setpath(e.toString())
+            })
+            setword('')
+        }
     }
     const handleChange = (event) =>{
         setword(event.target.value)
+        sethandling(false)
+        setpath(null)
     }
     return ( 
         <div className="interface">
@@ -44,7 +51,7 @@ const Interface = () => {
                 <FaSearch size={20} color='black' />
                 </div>
                </div>
-                <div className="result">
+                <div className={`result${handling?' animate':''}`} >
                     {path && <WordList path={path}/>}
                 </div>
             </div>
